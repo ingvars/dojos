210a4dbeafd5dc5c9571c47871afb06ea1c5a8da
Steffensen, Ingvar <ingvar.steffensen@gmail.com>
2018-01-09 22:17:20 +0100

    Create class responsible for evaluation + test class

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FizzBuzz
+﻿namespace FizzBuzz
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var fizzBuzzEvaluator = new FizzBuzzEvaluator();
+
+            for (int i=1; i<100; i++)
+            {
+                
+            }
         }
     }
 }
---
+++ FizzBuzz/Tests/FizzBuzz.UnitTests/FizzBuzzEvaluatorTest.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace FizzBuzz.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzEvaluatorTest
+    {
+        
+    }
+}
