d12df1a9feb6995519e0c8b7b79145372bb46b52
Steffensen, Ingvar <ingvar.steffensen@gmail.com>
2018-01-10 09:50:12 +0100

    Empty console-app + test project

---
+++ FizzBuzzTemplate/FizzBuzzTemplate/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace FizzBuzzTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ FizzBuzzTemplate/Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("66eaf87f-33f3-4a94-b3c8-a6b4857c85c5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
